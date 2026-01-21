b7270a02c7dabab7598806edee1b9944cefed60b
Mr-Usty <uasetechstudio@gmail.com>
2026-01-21 10:57:31 -0800

    updated 2026 website

--- core/urls.py
+++ core/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('portfolio/graphics/', views.graphics_portfolio, name='graphics_portfolio'),
+    path('graphics/', views.graphics_portfolio, name='graphics_portfolio'),
 
     
 
